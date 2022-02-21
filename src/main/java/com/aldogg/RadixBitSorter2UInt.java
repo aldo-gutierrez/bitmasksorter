d5f8451cae7d4acc22d7a5d26aa99b20cb34a60e
aldo-gutierrez <aldo.agg@gmail.com>
2022-02-21 14:17:52 -0400

    Performance Improvement of RadixBitSorter2UInt

--- src/main/java/com/aldogg/RadixBitSorter2UInt.java
+++ src/main/java/com/aldogg/RadixBitSorter2UInt.java
@@ -23,7 +23,7 @@ public class RadixBitSorter2UInt extends RadixBitSorterUInt {
             int sortMask1 = getMask(kListI);
             int bits = 1;
             int imm = 0;
-            for (int j = 1; j <= 7; j++) {
+            for (int j = 1; j <= 11; j++) { //11bits looks faster than 8 on AMD 4800H, 15 is slower
                 if (i - j >= kIndexEnd) {
                     int kListIm1 = kList[i - j];
                     if (kListIm1 == kListI + j) {
@@ -40,36 +40,56 @@ public class RadixBitSorter2UInt extends RadixBitSorterUInt {
             if (bits == 1) {
                 stablePartition(list, start, end, sortMask1, aux);
             } else {
-                stablePartition(list, start, end, sortMask1, aux, bits, 32 - bits - kListI, 32 - bits);
+                int lengthBitsToNumber = (int) Math.pow(2, bits);
+                int shiftLeft =  32 - bits - kListI;
+                int shiftRight = 32 - bits;
+                boolean eqShift = shiftLeft == shiftRight;
+                if (eqShift) {
+                    stablePartition1(list, start, end, lengthBitsToNumber, aux, sortMask1);
+                } else {
+                    stablePartition2(list, start, end, lengthBitsToNumber, aux, shiftLeft, shiftRight);
+                }
             }
         }
     }
 
-    protected void stablePartition(final int[] list, final int start, final int end, int sortMask, final int[] aux2, final int bits, final int shiftLeft, final int shiftRight) {
-        int lengthBitsToNumber = (int) Math.pow(2, bits);
-
+    private void stablePartition2(int[] list, int start, int end, int lengthBitsToNumber, int[] aux, int shiftLeft, int shiftRight) {
         int[] leftX = new int[lengthBitsToNumber];
         int[] count = new int[lengthBitsToNumber];
-
-        sortMask = shiftPre(sortMask, shiftLeft, shiftRight);
         for (int i = start; i < end; i++) {
             int element = list[i];
-            int elementShift = shiftPre(element, shiftLeft, shiftRight);
-            int elementMasked = elementShift & sortMask;
-            count[elementMasked]++;
+            int elementShiftMasked = shiftPre(element, shiftLeft, shiftRight);
+            count[elementShiftMasked]++;
         }
         for (int i = 1; i < lengthBitsToNumber; i++) {
             leftX[i] = leftX[i - 1] + count[i - 1];
         }
-
         for (int i = start; i < end; i++) {
             int element = list[i];
-            int elementShift = shiftPre(element, shiftLeft, shiftRight);
-            int elementMasked = elementShift & sortMask;
-            aux2[leftX[elementMasked]] = element;
-            leftX[elementMasked]++;
+            int elementShiftMasked = shiftPre(element, shiftLeft, shiftRight);
+            aux[leftX[elementShiftMasked]] = element;
+            leftX[elementShiftMasked]++;
         }
-        System.arraycopy(aux2, 0, list, start, end - start);
+        System.arraycopy(aux, 0, list, start, end - start);
+    }
+
+    private void stablePartition1(int[] list, int start, int end, int lengthBitsToNumber, int[] aux, int sortMask) {
+        int[] leftX = new int[lengthBitsToNumber];
+        int[] count = new int[lengthBitsToNumber];
+        for (int i = start; i < end; i++) {
+            int elementShiftMasked = list[i] & sortMask;
+            count[elementShiftMasked]++;
+        }
+        for (int i = 1; i < lengthBitsToNumber; i++) {
+            leftX[i] = leftX[i - 1] + count[i - 1];
+        }
+        for (int i = start; i < end; i++) {
+            int element = list[i];
+            int elementShiftMasked = element & sortMask;
+            aux[leftX[elementShiftMasked]] = element;
+            leftX[elementShiftMasked]++;
+        }
+        System.arraycopy(aux, 0, list, start, end - start);
     }
 
     private int shiftPre(int i, int shiftLeft, int shiftRight) {
