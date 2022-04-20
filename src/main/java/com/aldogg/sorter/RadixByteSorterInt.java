996ccedcf97dad53fa4b0ebcdf79079c34836e6d
aldo-gutierrez <aldo.agg@gmail.com>
2022-04-20 16:01:12 -0400

    calculateBitMaskOptimization RadixByteSorterInt optimized for negative numbers

--- src/main/java/com/aldogg/sorter/RadixByteSorterInt.java
+++ src/main/java/com/aldogg/sorter/RadixByteSorterInt.java
@@ -9,7 +9,7 @@ public class RadixByteSorterInt implements IntSorter {
 
     boolean unsigned = false;
     boolean stable = true;
-    boolean calculateBitMaskOptimization = true;
+    boolean calculateBitMaskOptimization = false;
 
     @Override
     public boolean isUnsigned() {
@@ -54,70 +54,101 @@ public class RadixByteSorterInt implements IntSorter {
             if (kList.length == 0) {
                 return;
             }
-            s0 = (mask & 0xFF) != 0;
-            s8 = (mask & 0xFF00) != 0;
-            s16 = (mask & 0xFF0000) != 0;
-            s24 = (mask & 0xFF000000) != 0;
+            if (kList[0] == 31 && !isUnsigned()) { //there are negative numbers and positive numbers
+                int sortMask = BitSorterUtils.getMaskBit(kList[0]);
+                int finalLeft = isUnsigned()
+                        ? IntSorterUtils.partitionNotStable(array, start, end, sortMask)
+                        : IntSorterUtils.partitionReverseNotStable(array, start, end, sortMask);
+                if (finalLeft - start > 1) { //sort negative numbers
+                    //int[] aux = new int[finalLeft - start];
+                    maskParts = getMaskBit(array, start, finalLeft);
+                    mask = maskParts[0] & maskParts[1];
+                    s0 = (mask & 0xFF) != 0;
+                    s8 = (mask & 0xFF00) != 0;
+                    s16 = (mask & 0xFF0000) != 0;
+                    s24 = (mask & 0xFF000000) != 0;
+                    sortBytes(array, start, finalLeft, s0, s8, s16, s24);
+                }
+                if (end - finalLeft > 1) { //sort positive numbers
+                    //int[] aux = new int[end - finalLeft];
+                    maskParts = getMaskBit(array, finalLeft, end);
+                    mask = maskParts[0] & maskParts[1];
+                    s0 = (mask & 0xFF) != 0;
+                    s8 = (mask & 0xFF00) != 0;
+                    s16 = (mask & 0xFF0000) != 0;
+                    s24 = (mask & 0xFF000000) != 0;
+                    sortBytes(array, finalLeft, end, s0, s8, s16, s24);
+                }
+                return;
+            }  else {
+                s0 = (mask & 0xFF) != 0;
+                s8 = (mask & 0xFF00) != 0;
+                s16 = (mask & 0xFF0000) != 0;
+                s24 = (mask & 0xFF000000) != 0;
+            }
         }
+        sortBytes(array, start, end, s0, s8, s16, s24);
+    }
 
-        int length = array.length;
+    private void sortBytes(int[] array, int start, int end, boolean s0, boolean s8, boolean s16, boolean s24) {
+        int length = end - start;
         int[] aux = new int[length];
         int[] leftX = new int[256];
         if (s0) {
             int[] count = new int[256];
-            for (int i = 0; i < length; i++) {
+            for (int i = start; i < end; i++) {
                 count[array[i] & 0xFF]++;
             }
             leftX[0] = 0;
             for (int i = 1; i < 256; i++) {
                 leftX[i] = leftX[i - 1] + count[i - 1];
             }
-            for (int i = 0; i < length; i++) {
+            for (int i = start; i < end; i++) {
                 int element = array[i];
                 int elementShiftMasked = element & 0xFF;
                 aux[leftX[elementShiftMasked]] = element;
                 leftX[elementShiftMasked]++;
             }
-            System.arraycopy(aux, 0, array, 0, length);
+            System.arraycopy(aux, 0, array, start, length);
         }
         if (s8){
             int[] count = new int[256];
-            for (int i = 0; i < length; i++) {
+            for (int i = start; i < end; i++) {
                 count[array[i] >> 8 & 0xFF]++;
             }
             leftX[0] = 0;
             for (int i = 1; i < 256; i++) {
                 leftX[i] = leftX[i - 1] + count[i - 1];
             }
-            for (int i = 0; i < length; i++) {
+            for (int i = start; i < end; i++) {
                 int element = array[i];
                 int elementShiftMasked = element >> 8 & 0xFF;
                 aux[leftX[elementShiftMasked]] = element;
                 leftX[elementShiftMasked]++;
             }
-            System.arraycopy(aux, 0, array, 0, length);
+            System.arraycopy(aux, 0, array, start, length);
         }
         if (s16) {
             int[] count = new int[256];
-            for (int i = 0; i < length; i++) {
+            for (int i = start; i < end; i++) {
                 count[array[i] >> 16 & 0xFF]++;
             }
             leftX[0] = 0;
             for (int i = 1; i < 256; i++) {
                 leftX[i] = leftX[i - 1] + count[i - 1];
             }
-            for (int i = 0; i < length; i++) {
+            for (int i = start; i < end; i++) {
                 int element = array[i];
                 int elementShiftMasked = element >> 16 & 0xFF;
                 aux[leftX[elementShiftMasked]] = element;
                 leftX[elementShiftMasked]++;
             }
-            System.arraycopy(aux, 0, array, 0, length);
+            System.arraycopy(aux, 0, array, start, length);
 
         }
         if (s24) {
             int[] count = new int[256];
-            for (int i = 0; i < length; i++) {
+            for (int i = start; i < end; i++) {
                 count[array[i] >>24 & 0xFF]++;
             }
             leftX[0] = 0;
@@ -125,21 +156,21 @@ public class RadixByteSorterInt implements IntSorter {
                 leftX[i] = leftX[i - 1] + count[i - 1];
             }
             int lengthPositive = leftX[128];
-            for (int i = 0; i < length; i++) {
+            for (int i = start; i < end; i++) {
                 int element = array[i];
                 int elementShiftMasked = element>>24 & 0xFF;
                 aux[leftX[elementShiftMasked]] = element;
                 leftX[elementShiftMasked]++;
             }
             if (unsigned) {
-                System.arraycopy(aux, 0, array, 0, length);
+                System.arraycopy(aux, 0, array, start, length);
             } else {
                 if (lengthPositive < length) {
                     int lengthNegative = length - lengthPositive;
-                    System.arraycopy(aux, lengthPositive, array, 0, lengthNegative);
-                    System.arraycopy(aux, 0, array, lengthNegative, lengthPositive);
+                    System.arraycopy(aux, lengthPositive, array, start, lengthNegative);
+                    System.arraycopy(aux, 0, array, start + lengthNegative, lengthPositive);
                 } else {
-                    System.arraycopy(aux, 0, array, 0, length);
+                    System.arraycopy(aux, 0, array, start, length);
                 }
             }
         }
