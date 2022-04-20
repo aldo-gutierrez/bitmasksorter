d323d7e35960385d74397b7095ebec99cec13d99
aldo-gutierrez <aldo.agg@gmail.com>
2022-04-20 15:02:50 -0400

    added optional optimization to RadixByteSorterInt

--- src/main/java/com/aldogg/sorter/RadixByteSorterInt.java
+++ src/main/java/com/aldogg/sorter/RadixByteSorterInt.java
@@ -9,6 +9,7 @@ public class RadixByteSorterInt implements IntSorter {
 
     boolean unsigned = false;
     boolean stable = true;
+    boolean calculateBitMaskOptimization = true;
 
     @Override
     public boolean isUnsigned() {
@@ -24,6 +25,10 @@ public class RadixByteSorterInt implements IntSorter {
         return stable;
     }
 
+    public void setCalculateBitMaskOptimization(boolean calculateBitMaskOptimization) {
+        this.calculateBitMaskOptimization = calculateBitMaskOptimization;
+    }
+
     @Override
     public void sort(int[] array) {
         if (array.length < 2) {
@@ -37,10 +42,28 @@ public class RadixByteSorterInt implements IntSorter {
         }
         if (ordered != AnalysisResult.UNORDERED) return;
 
+        boolean s0 = true;
+        boolean s8 = true;
+        boolean s16 = true;
+        boolean s24 = true;
+        if (calculateBitMaskOptimization) {
+            int[] maskParts = getMaskBit(array, start, end);
+            int mask = maskParts[0] & maskParts[1];
+            int[] kList = getMaskAsList(mask);
+
+            if (kList.length == 0) {
+                return;
+            }
+            s0 = (mask & 0xFF) != 0;
+            s8 = (mask & 0xFF00) != 0;
+            s16 = (mask & 0xFF0000) != 0;
+            s24 = (mask & 0xFF000000) != 0;
+        }
+
         int length = array.length;
         int[] aux = new int[length];
         int[] leftX = new int[256];
-        {
+        if (s0) {
             int[] count = new int[256];
             for (int i = 0; i < length; i++) {
                 count[array[i] & 0xFF]++;
@@ -57,10 +80,10 @@ public class RadixByteSorterInt implements IntSorter {
             }
             System.arraycopy(aux, 0, array, 0, length);
         }
-        for (int shift =8; shift <= 16; shift+=8) {
+        if (s8){
             int[] count = new int[256];
             for (int i = 0; i < length; i++) {
-                count[array[i] >>shift & 0xFF]++;
+                count[array[i] >> 8 & 0xFF]++;
             }
             leftX[0] = 0;
             for (int i = 1; i < 256; i++) {
@@ -68,13 +91,31 @@ public class RadixByteSorterInt implements IntSorter {
             }
             for (int i = 0; i < length; i++) {
                 int element = array[i];
-                int elementShiftMasked = element>>shift & 0xFF;
+                int elementShiftMasked = element >> 8 & 0xFF;
                 aux[leftX[elementShiftMasked]] = element;
                 leftX[elementShiftMasked]++;
             }
             System.arraycopy(aux, 0, array, 0, length);
         }
-        {
+        if (s16) {
+            int[] count = new int[256];
+            for (int i = 0; i < length; i++) {
+                count[array[i] >> 16 & 0xFF]++;
+            }
+            leftX[0] = 0;
+            for (int i = 1; i < 256; i++) {
+                leftX[i] = leftX[i - 1] + count[i - 1];
+            }
+            for (int i = 0; i < length; i++) {
+                int element = array[i];
+                int elementShiftMasked = element >> 16 & 0xFF;
+                aux[leftX[elementShiftMasked]] = element;
+                leftX[elementShiftMasked]++;
+            }
+            System.arraycopy(aux, 0, array, 0, length);
+
+        }
+        if (s24) {
             int[] count = new int[256];
             for (int i = 0; i < length; i++) {
                 count[array[i] >>24 & 0xFF]++;
