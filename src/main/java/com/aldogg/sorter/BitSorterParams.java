3480588f20a22ecfffb976b3522609d7f4dbe8e6
aldo-gutierrez <aldo.agg@gmail.com>
2022-12-20 19:45:03 -0400

    MAX_BITS_RADIX_SORT initialized based on number of cores as L1 cache size is not possible to query from Java

--- src/main/java/com/aldogg/sorter/BitSorterParams.java
+++ src/main/java/com/aldogg/sorter/BitSorterParams.java
@@ -5,8 +5,23 @@ import java.security.InvalidParameterException;
 public class BitSorterParams {
     public static final int VERY_SMALL_N_SIZE = 16;
 
-    //11bits looks faster than 8 on AMD 4800H, 15 is slower
-    public static final int MAX_BITS_RADIX_SORT = 11;
+    public static final int MAX_BITS_RADIX_SORT;
+
+    static {
+        //looks like MAX_BITS_RADIX_SORT depends on cache size, an approximation is to use the number of cores
+        int cores = Runtime.getRuntime().availableProcessors();
+        if (cores <= 4) {
+            //8bits looks faster on Single Thread on Core i5-5200U
+            MAX_BITS_RADIX_SORT = 8;
+        } else if (cores <=6) {
+            MAX_BITS_RADIX_SORT = 9;
+        } else if (cores <=8 ) {
+            MAX_BITS_RADIX_SORT = 10;
+        } else {
+            //11bits looks faster than 8 on AMD 4800H, 15 is slower
+            MAX_BITS_RADIX_SORT = 11;
+        }
+    }
 
     private int countingSortBits = 16;
 
