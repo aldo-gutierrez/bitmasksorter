7c14651a58cb985ecd631cd57374c024a1672f1e
aldo-gutierrez <aldo.agg@gmail.com>
2023-04-24 12:53:25 -0400

    some fixes

--- src/main/java/com/aldogg/sorter/BitSorterParams.java
+++ src/main/java/com/aldogg/sorter/BitSorterParams.java
@@ -13,13 +13,15 @@ public class BitSorterParams {
         if (cores <= 4) {
             //8bits looks faster on Single Thread on Core i5-5200U
             MAX_BITS_RADIX_SORT = 8;
-        } else if (cores <=6) {
+        } else if (cores <= 6) {
             MAX_BITS_RADIX_SORT = 9;
-        } else if (cores <=8 ) {
+        } else if (cores <= 8) {
             MAX_BITS_RADIX_SORT = 10;
-        } else {
+        } else if (cores <= 16) {
             //11bits looks faster than 8 on AMD 4800H, 15 is slower
             MAX_BITS_RADIX_SORT = 11;
+        } else {
+            MAX_BITS_RADIX_SORT = 12;
         }
     }
 
